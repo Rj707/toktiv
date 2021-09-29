0e19526d290be89801e3b7aba65a27071bb06af9
Saad <>
2021-09-29 12:09:50 +0500

    code formatting

--- toktiv-main/toktiv/Views/Login/LoginViewController.swift
+++ toktiv-main/toktiv/Views/Login/LoginViewController.swift
@@ -22,59 +22,13 @@ class LoginViewController: UIViewController, UITextFieldDelegate
     var stateManager = StateManager.shared
     var backgroundTaskIdentifier: UIBackgroundTaskIdentifier?
     
-    
     //MARK: - View Lifecycle
+    
     override func viewDidLoad()
     {
-        
         super.viewDidLoad()
         
-        if let dataToken = UserDefaults.standard.data(forKey: AppConstants.USER_ACCESS_TOKEN), let _ = try? JSONDecoder().decode(LoginTokenModel.self, from: dataToken),  let dataProfile = UserDefaults.standard.data(forKey: AppConstants.USER_PROFILE_MODEL), let userProfileModel = try? JSONDecoder().decode(LoginUserModel.self, from: dataProfile)
-        {
-            
-            if let validDeviceData = UserDefaults.standard.data(forKey: kCachedDeviceToken), let validAccessToken = userProfileModel.twillioToken
-            {
-                self.handleAccessTokenExpiry(validAccessToken)
-                
-                TwilioVoice.register(accessToken: validAccessToken, deviceToken: validDeviceData)
-                { (error) in
-                    
-                    if let error = error
-                    {
-                        NSLog("LOGIN: An error occurred while registering: \(error.localizedDescription)")
-                    }
-                    else
-                    {
-                        NSLog("LOGIN: Successfully registered for VoIP push notifications.")
-                    }
-                }
-            }
-            
-            self.stateManager.loginViewModel.userProfile = userProfileModel
-            self.stateManager.loginViewModel.defaultPhoneNumber = self.stateManager.loginViewModel.userProfile?.twilioNum ?? ""
-            
-            MessagingManager.sharedManager().connectClientWithCompletion
-            { (success, error) in
-                
-                if success
-                {
-                    MessagingManager.sharedManager().registerChatClientWith(deviceToken: (UIApplication.shared.delegate as! AppDelegate).updatedPushToken ?? Data.init())
-                    { (success) in
-                        
-                        if success
-                        {
-                        }
-                        else
-                        {
-                        }
-                    }
-                }
-            }
-            
-            // Navigate to Dashboard
-            self.moveToDashbnoard(animated: false)
-
-        }
+        setupFlow()
         
         usernameTextField.text = "Sameer"
         passwordTextField.text = "apss2021"
@@ -85,108 +39,29 @@ class LoginViewController: UIViewController, UITextFieldDelegate
         self.view.endEditing(true)
     }
     
+    //MARK: - Helper Functions
     
-    //MARK: - IBActions
-    
-    @IBAction func loginButtonPressed(_ sender:UIButton)
+    func setupFlow()
     {
-        guard let username = self.usernameTextField.text, username.count > 1
-        else
+        if let dataToken = UserDefaults.standard.data(forKey: AppConstants.USER_ACCESS_TOKEN), let _ = try? JSONDecoder().decode(LoginTokenModel.self, from: dataToken),  let dataProfile = UserDefaults.standard.data(forKey: AppConstants.USER_PROFILE_MODEL), let userProfileModel = try? JSONDecoder().decode(LoginUserModel.self, from: dataProfile)
         {
-            NotificationBanner(title: nil, subtitle: "Please enter valid Username.", leftView: nil, rightView: nil, style: .warning, colors: nil).show()
-            return
-        }
-        guard let password = self.passwordTextField.text, password.count > 1
-        else
-        {
-            NotificationBanner(title: nil, subtitle: "Please enter valid Password.", leftView: nil, rightView: nil, style: .warning, colors: nil).show()
-            return
-        }
-        
-        MBProgressHUD.showAdded(to: self.view, animated: true)
-        stateManager.loginViewModel.getAccessToken(with: username, password: password)
-        { (response, error) in
-            
-            DispatchQueue.main.async
+            if let validDeviceData = UserDefaults.standard.data(forKey: kCachedDeviceToken), let validAccessToken = userProfileModel.twillioToken
             {
-                if let validResponse = response, let validToken = validResponse.accessToken, let dataObject = try? JSONEncoder().encode(validResponse)
-                {
-                    
-                    UserDefaults.standard.setValue(dataObject, forKey: AppConstants.USER_ACCESS_TOKEN)
-                    UserDefaults.standard.synchronize()
-                    
-                    self.stateManager.loginViewModel.getUserProfile(with: validToken, username: username, password: password)
-                    { (response, error) in
-                        
-                        MBProgressHUD.hide(for: self.view, animated: true)
-                        
-                        if let validResponse = response
-                        {
-                            if let dataObject = try? JSONEncoder().encode(validResponse)
-                            {
-                                UserDefaults.standard.setValue(dataObject, forKey: AppConstants.USER_PROFILE_MODEL)
-                                UserDefaults.standard.synchronize()
-                            }
-                            
-                            if let validDeviceData = UserDefaults.standard.data(forKey: kCachedDeviceToken), let validAccessToken = validResponse.twillioToken
-                            {
-                                
-                                self.handleAccessTokenExpiry(validAccessToken)
-                                
-                                TwilioVoice.register(accessToken: validAccessToken, deviceToken: validDeviceData)
-                                { (error) in
-                                    
-                                    if let error = error
-                                    {
-                                        NSLog("LOGIN: An error occurred while registering: \(error.localizedDescription)")
-                                    }
-                                    else
-                                    {
-                                        NSLog("LOGIN: Successfully registered for VoIP push notifications.")
-                                        StateManager.shared.accessToken = validAccessToken
-                                    }
-                                }
-                            }
-                            
-                            
-                            MessagingManager.sharedManager().connectClientWithCompletion
-                            { (success, error) in
-                                
-                                MessagingManager.sharedManager().registerChatClientWith(deviceToken: (UIApplication.shared.delegate as! AppDelegate).updatedPushToken ?? Data.init())
-                                { (success) in
-                                    
-                                    if success
-                                    {
-                                    }
-                                    else
-                                    {
-                                    }
-                                }
-                            }
-                            
-                            self.stateManager.setupWorkerStatus(validResponse.twilioClientStatus ?? "")
-                            self.stateManager.loginViewModel.userProfile = validResponse
-                            self.stateManager.loginViewModel.defaultPhoneNumber = self.stateManager.loginViewModel.userProfile?.twilioNum ?? ""
-                            self.moveToDashbnoard(animated: true)
-                        }
-                        else
-                        {
-                            NotificationBanner(title: nil, subtitle: "Unable to login, Please try again", leftView: nil, rightView: nil, style: .danger, colors: nil).show()
-                        }
-                    }
-                }
-                else
-                {
-                    MBProgressHUD.hide(for: self.view, animated: true)
-                    // error
-                    NotificationBanner(title: nil, subtitle: "Login Failed: \(response?.error ?? "Something went wrong")", leftView: nil, rightView: nil, style: .danger, colors: nil).show()
-                }
+                self.handleAccessTokenExpiry(validAccessToken)
+                
+                self.registerAtTwilioWith(accessToken: validAccessToken, andDeviceToken: validDeviceData)
             }
+            
+            self.stateManager.loginViewModel.userProfile = userProfileModel
+            
+            self.stateManager.loginViewModel.defaultPhoneNumber = self.stateManager.loginViewModel.userProfile?.twilioNum ?? ""
+            
+            self.connectTwilioChatClient()
+            
+            // Navigate to Dashboard
+            self.moveToDashbnoard(animated: false)
         }
     }
-    
-    
-    //MARK: - Helper Functions
     
     func handleAccessTokenExpiry(_ accessToken:String)
     {
@@ -215,6 +90,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate
             if expDate <= currentDate
             {
                 let providerCode = self.stateManager.loginViewModel.userProfile?.providerCode ?? ""
+                
                 self.stateManager.loginViewModel.getTwilioAccessToken(providerCode)
                 { (respose, error) in
                     
@@ -222,21 +98,8 @@ class LoginViewController: UIViewController, UITextFieldDelegate
                     {
                         print("Valid Refresh Token: \(validAccessToken)")
                         
-                        TwilioVoice.register(accessToken: validAccessToken, deviceToken: validDeviceData)
-                        { (error) in
-                            
-                            if let error = error
-                            {
-                                NSLog("LOGIN: An error occurred while registering: \(error.localizedDescription)")
-                            }
-                            else
-                            {
-                                NSLog("LOGIN: Successfully registered for VoIP push notifications.")
-                                self.expDate = Date().addingTimeInterval(60*60)
-                            }
-                        }
+                        self.registerAtTwilioWith(accessToken: validAccessToken, andDeviceToken: validDeviceData)
                     }
-                    
                 }
             }
         }
@@ -247,6 +110,135 @@ class LoginViewController: UIViewController, UITextFieldDelegate
         if let controller = self.storyboard?.instantiateViewController(withIdentifier: "DashbaordViewController") as? DashbaordViewController
         {
             self.navigationController?.pushViewController(controller, animated: animated)
+        }
+    }
+    
+    //MARK: - IBActions
+    
+    @IBAction func loginButtonPressed(_ sender:UIButton)
+    {
+        guard let username = self.usernameTextField.text, username.count > 1
+        else
+        {
+            NotificationBanner(title: nil, subtitle: "Please enter valid Username.", leftView: nil, rightView: nil, style: .warning, colors: nil).show()
+            return
+        }
+        guard let password = self.passwordTextField.text, password.count > 1
+        else
+        {
+            NotificationBanner(title: nil, subtitle: "Please enter valid Password.", leftView: nil, rightView: nil, style: .warning, colors: nil).show()
+            return
+        }
+        
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
+        self.getAccessTokenFroServerAgainst(username: username, password: password)
+    }
+    
+    //MARK: - APIs
+    
+    func getAccessTokenFroServerAgainst(username: String, password: String)
+    {
+        stateManager.loginViewModel.getAccessToken(with: username, password: password)
+        { (response, error) in
+            
+            DispatchQueue.main.async
+            {
+                if let validResponse = response, let validToken = validResponse.accessToken, let dataObject = try? JSONEncoder().encode(validResponse)
+                {
+                    
+                    UserDefaults.standard.setValue(dataObject, forKey: AppConstants.USER_ACCESS_TOKEN)
+                    UserDefaults.standard.synchronize()
+                    
+                    self.getUserProfileAgainstAccessToken(validToken: validToken, username: username, password: password)
+                }
+                else
+                {
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    // error
+                    NotificationBanner(title: nil, subtitle: "Login Failed: \(response?.error ?? "Something went wrong")", leftView: nil, rightView: nil, style: .danger, colors: nil).show()
+                }
+            }
+        }
+    }
+    
+    func getUserProfileAgainstAccessToken(validToken: String, username: String, password: String)
+    {
+        self.stateManager.loginViewModel.getUserProfile(with: validToken, username: username, password: password)
+        { (response, error) in
+            
+            MBProgressHUD.hide(for: self.view, animated: true)
+            
+            if let validResponse = response
+            {
+                if let dataObject = try? JSONEncoder().encode(validResponse)
+                {
+                    UserDefaults.standard.setValue(dataObject, forKey: AppConstants.USER_PROFILE_MODEL)
+                    UserDefaults.standard.synchronize()
+                }
+                
+                if let validDeviceData = UserDefaults.standard.data(forKey: kCachedDeviceToken), let validAccessToken = validResponse.twillioToken
+                {
+                    self.handleAccessTokenExpiry(validAccessToken)
+                    
+                    self.registerAtTwilioWith(accessToken: validAccessToken, andDeviceToken: validDeviceData)
+                }
+                
+                self.connectTwilioChatClient()
+                
+                self.stateManager.setupWorkerStatus(validResponse.twilioClientStatus ?? "")
+                self.stateManager.loginViewModel.userProfile = validResponse
+                self.stateManager.loginViewModel.defaultPhoneNumber = self.stateManager.loginViewModel.userProfile?.twilioNum ?? ""
+                self.moveToDashbnoard(animated: true)
+            }
+            else
+            {
+                NotificationBanner(title: nil, subtitle: "Unable to login, Please try again", leftView: nil, rightView: nil, style: .danger, colors: nil).show()
+            }
+        }
+    }
+
+    //MARK: - Twilio
+    
+    func registerAtTwilioWith(accessToken: String, andDeviceToken deviceToken: Data)
+    {
+        TwilioVoice.register(accessToken: accessToken, deviceToken: deviceToken)
+        { (error) in
+            
+            if let error = error
+            {
+                NSLog("LOGIN: An error occurred while registering: \(error.localizedDescription)")
+            }
+            else
+            {
+                NSLog("LOGIN: Successfully registered for VoIP push notifications.")
+            }
+        }
+    }
+    
+    func connectTwilioChatClient()
+    {
+        MessagingManager.sharedManager().connectClientWithCompletion
+        { (success, error) in
+            
+            if success
+            {
+                self.registerTwilioChatClientWithDeviceToken()
+            }
+        }
+    }
+    
+    func registerTwilioChatClientWithDeviceToken()
+    {
+        MessagingManager.sharedManager().registerChatClientWith(deviceToken: (UIApplication.shared.delegate as! AppDelegate).updatedPushToken ?? Data.init())
+        { (success) in
+            
+            if success
+            {
+            }
+            else
+            {
+            }
         }
     }
     
